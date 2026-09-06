9153447156969b727293b00cd64985b3fede847c
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-09-06 16:47:13 +0530

    register authentication email service

--- 3d-commerce-api/src/auth/auth.module.ts
+++ 3d-commerce-api/src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthEmailService } from './email.service';
 import { AuthGuard } from './guards/auth.guard';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, AuthGuard],
+  providers: [AuthService, AuthEmailService, AuthGuard],
   exports: [AuthService, AuthGuard],
 })
 export class AuthModule {}
