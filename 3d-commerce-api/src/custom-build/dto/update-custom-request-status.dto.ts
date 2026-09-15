590277732b4af9c6849e1803e8d79ba16cd8f3a5
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-09-15 11:41:12 +0530

    refine(shop): expand search and remove smart label

--- 3d-commerce-api/src/custom-build/dto/update-custom-request-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { CustomRequestStatus } from '@prisma/client';
-
-export class UpdateCustomRequestStatusDto {
-  @IsEnum(CustomRequestStatus)
-  status: CustomRequestStatus;
-}
