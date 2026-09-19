cf817787c27bd43e7af9148a5e8f3ae3c8c71fcd
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-09-19 12:18:42 +0530

    remove unused custom processing dependency

--- 3d-commerce-api/src/custom-build/custom-build.module.ts
+++ 3d-commerce-api/src/custom-build/custom-build.module.ts
@@ -4,7 +4,6 @@ import { AuthModule } from '../auth/auth.module';
 import { CartModule } from '../cart/cart.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { PrismaModule } from '../prisma/prisma.module';
-import { ProcessingJobsModule } from '../processing-jobs/processing-jobs.module';
 import { StorageModule } from '../storage/storage.module';
 
 import { CustomBuildController } from './custom-build.controller';
@@ -18,7 +17,6 @@ import { CustomBuildService } from './custom-build.service';
     CartModule,
     NotificationsModule,
     AuthModule,
-    ProcessingJobsModule,
   ],
 
   controllers: [
