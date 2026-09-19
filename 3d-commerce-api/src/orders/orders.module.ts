67e2d96d699f3db1c187490c0c7003d6052253a0
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-09-19 14:53:59 +0530

    fix(orders): register Razorpay provider for refunds

--- 3d-commerce-api/src/orders/orders.module.ts
+++ 3d-commerce-api/src/orders/orders.module.ts
@@ -7,11 +7,12 @@ import { OrdersService } from './orders.service';
 import { ReturnsService } from './returns.service';
 import { AuthModule } from '../auth/auth.module';
 import { OrdersExpirationScheduler } from './orders-expiration.scheduler';
+import { RazorpayService } from '../payments/razorpay.service';
 
 @Module({
   imports: [PrismaModule, NotificationsModule, PricingModule, AuthModule],
   controllers: [OrdersController],
-  providers: [OrdersService, ReturnsService, OrdersExpirationScheduler],
+  providers: [OrdersService, ReturnsService, OrdersExpirationScheduler, RazorpayService],
   exports: [OrdersService, ReturnsService],
 })
 export class OrdersModule {}
