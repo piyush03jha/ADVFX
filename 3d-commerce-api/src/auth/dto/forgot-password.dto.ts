9e1f09fdc48717d9759fb10fb29a048783e2e6cb
piyush Jha <153450127+piyush03jha@users.noreply.github.com>
2026-09-20 21:48:21 +0530

    security(api): require CAPTCHA on password recovery

--- 3d-commerce-api/src/auth/dto/forgot-password.dto.ts
+++ 3d-commerce-api/src/auth/dto/forgot-password.dto.ts
@@ -1,7 +1,17 @@
-import { IsEmail, MaxLength } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class ForgotPasswordDto {
   @IsEmail()
   @MaxLength(320)
   email!: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(4096)
+  captchaToken!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(2)
+  captchaAnswer?: string;
 }
